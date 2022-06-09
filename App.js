1af88b28a2fff191129dd0a8d9682b49c7036f24
Ong Dao Nan <ongdaonan@Ongs-MacBook-Pro.local>
2022-06-09 13:39:39 +0800

    Hello

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  let [count, setCount] = useState(0);
+
+  function buttonPressed() {
+    console.log('You pressed me ouch!');
+    setCount(count + 1);
+  }
+
+  function renderEncouragingText() {
+    if (count >= 10 && count < 20) {
+      return 'Keep going!';
+    } if (count >= 20 && count < 30) {
+      return 'Take a break!';
+    } if (count >= 30) {
+      return 'STOP!!!';
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{count}</Text>
+      <TouchableOpacity style={styles.button} onPress={buttonPressed}>
+        <Text style={styles.buttonText}>Press me!</Text>
+      </TouchableOpacity>
+      <Text style={styles.encouragingText}>{renderEncouragingText()}</Text>
     </View>
   );
 }
@@ -13,8 +33,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  encouragingText: {
+    marginTop: 50,
+    color: 'darkgrey',
+  },
+  button: {
+    backgroundColor: 'olive',
+    padding: 20,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 40,
   },
 });
